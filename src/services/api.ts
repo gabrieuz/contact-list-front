4880627da494a38673d92b4f39064022a721249e
Gabriel Martins <lima.gabriel@sou.ufac.br>
2023-02-06 00:05:00 -0500

    fix: minor edits in services

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "https://frontend-challenge-7bu3nxh76a-uc.a.run.app",
+	baseURL: "https://localhost:3000",
 	timeout: 10000,
 });
 
